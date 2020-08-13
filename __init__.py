f414230b61601c7443f74312160068b844a5ec76
sumitkhamar22 <69629401+sumitkhamar22@users.noreply.github.com>
2020-08-13 16:07:49 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""Free Google Translate API for Python. Translates totally free of charge."""
+__all__ = 'Translator',
+__version__ = '3.0.0'
+
+
+from dragoman.client import Translator
+from dragoman.constants import LANGCODES, LANGUAGES  # noqa
